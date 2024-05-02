4b6fdfd01c849e2b676952577a5f96dfebc35b95
Lialliane <lojain01k@gmail.com>
2024-05-02 13:29:44 +0300

    Update Credit.js

--- src/Credit.js
+++ src/Credit.js
@@ -5,18 +5,18 @@ export default function Credit() {
     <p className="credits">
       This project was coded by
       <a
-        href="https://www.shecodes.io/graduates/104683-lujain-alkhatib"
-        target="_blank"
+        href="https://www.shecodes.io/graduates/104683-lujain-alkhatib" rel="noreferrer"
+        target="_blank" 
       >
         Lujain Khatib
       </a>{" "}
       and is
-      <a href="https://github.com/Lialliane/Weather_app" target="_blank">
+      <a href="https://github.com/Lialliane/Weather_app" rel="noreferrer" target="_blank" >
         {" "}
         on GitHub
       </a>{" "}
       and
-      <a href="https://l-weather-app.netlify.app" target="_blank">
+      <a href="https://l-weather-app.netlify.app" rel="noreferrer" target="_blank">
         hosted on Netlify
       </a>
     </p>
