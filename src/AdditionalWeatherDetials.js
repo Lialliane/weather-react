36c57f3549c6a5cf3c0d74917ce488a7ea16650f
Lialliane <lojain01k@gmail.com>
2024-05-13 19:37:19 +0300

    Updated app to fully work in react.

---
+++ src/AdditionalWeatherDetials.js
@@ -0,0 +1,132 @@
+import { useState, React } from "react";
+import "./AdditionalWeatherDetials.css";
+
+export default function AdditionalWeatherDetials({weatherInfo}) {
+  let weatherCondition;
+  if(weatherInfo!=null){
+    weatherCondition = {
+      maxTemp : Math.round(weatherInfo.main.temp_max),
+      minTemp : Math.round(weatherInfo.main.temp_min),
+      feelsLike: Math.round(weatherInfo.main.feels_like),
+      humidity: weatherInfo.main.humidity,
+      pressure : weatherInfo.main.pressure,
+      wind: Math.round(weatherInfo.wind.speed)
+    };
+}
+else {
+  weatherCondition = {
+    maxTemp : 0,
+    minTemp : 0,
+    feelsLike: 0,
+    humidity: 0,
+    pressure : 0,
+    wind: 0
+  };
+}
+  let [opened, setOpened] = useState(null);
+
+  let additionalDetailsContainer = [<span> 
+  <p className="row align-items-end">
+    {" "}
+    <span className="col-md-5 col-sm-6 col-9">
+      {" "}
+      <span className="material-symbols-outlined">device_thermostat</span>
+      Temp: Max <span id="max-temp">{weatherCondition.maxTemp}°C</span>
+    </span>
+    <span className="col-md-3 col-6">
+      {" "}
+      , Min <span id="min-temp">{weatherCondition.minTemp}°C</span>
+    </span>{" "}
+    <span className="col-md-4 mt-md-0 col-6 mt-3">
+      , Feels like <span id="feels-like">{weatherCondition.feelsLike}°C</span>
+    </span>
+  </p>
+  <hr />
+  <p>
+    <span className="material-symbols-outlined">humidity_percentage</span>
+    Humidity: <span id="humidity">{weatherCondition.humidity}%</span>
+  </p>
+  <hr />
+  <p>
+    <span className="material-symbols-outlined">
+      swap_driving_apps_wheel
+    </span>
+    Pressure: <span id="pressure">{weatherCondition.pressure}</span>
+  </p>
+  <hr />
+  <p>
+    <span className="material-symbols-outlined">air</span>
+    Wind: <span id="wind-speed">{weatherCondition.wind} km/h</span>
+  </p>
+</span>];
+
+    function openContainer(){
+      if(opened==null)
+        setOpened(true);
+      
+      else
+        setOpened(!opened);
+    }
+
+
+  if(opened == null){
+    return (
+      <span>
+        <div className="additional-weather-details">
+        <button className="additional-details-button " >
+          Additional Details
+          <span className="material-symbols-outlined" id="arrow" onClick={openContainer}>
+            arrow_drop_down
+          </span>
+        </button>
+        <div id="additional-details-container">
+          {additionalDetailsContainer}
+        </div>
+      </div>
+        <div class = "emptyMovingContainer">
+        </div>
+      </span>
+    );
+  }
+  else if(opened){
+    return (
+    <span>
+      <div className="additional-weather-details">
+      <button className="additional-details-button " onClick={openContainer} >
+        Additional Details
+        <span className="material-symbols-outlined rotateArrowOpen" id="arrow">
+          arrow_drop_down
+        </span>
+      </button>
+      <div id="additional-details-container" class="animationContainer1">
+        {additionalDetailsContainer}
+      </div>
+    </div>
+      <div class = "emptyMovingContainer animationContainer2">
+      </div>
+    </span>
+  );}
+  else
+  return (
+    <span>
+      <div className="additional-weather-details">
+      <button className="additional-details-button " onClick={openContainer} >
+        Additional Details
+        <span className="material-symbols-outlined rotateArrowClose" id="arrow">
+          arrow_drop_down
+        </span>
+      </button>
+      <div id="additional-details-container" className="animationContainer1Reverse">
+        {additionalDetailsContainer}
+      </div>
+    </div>
+      <div class = "emptyMovingContainer animationContainer2Reverse">
+      </div>
+    </span>
+  );
+  
+}
+
+function AdditionalDetailsContainer({weatherInfo}){
+  return 
+}
--- src/App.js
+++ src/App.js
@@ -1,38 +1,133 @@
-import "./styles.css";
+import "./App.css";
 import FooterLogo from "./FooterLogo";
-import SearchForm from "./SearchForm";
 import Credit from "./Credit";
-import AddtionalDetialsContainer from "./AddtionalDetialsContainer";
+import AdditionalWeatherDetials from "./AdditionalWeatherDetials";
+import axios from "axios";
+import {useState } from "react";
+import FormatDate from './FormatDate';
+import WeatherIcon from "./WeatherIcon";
+
+
 
 export default function App() {
+  let [error, setError] = useState(null);
+  let [weatherInfo , setWeatherInfo]= useState(null);
+  let [firstApiCall, setFirstApiCall]= useState(true);
+  let [city, setCity] = useState('');
+
+
+
+  
+  let ipAddressURL = 'https://json.geoiplookup.io/';
+
+  if(firstApiCall){
+    axios.get(ipAddressURL).then(getCityWeather).catch(handleError);
+  }
+
+  function updateCity(event){
+      setError('');
+      setCity(event.target.value);
+  }
+
+  function searchCity(event){
+    event.preventDefault();
+    getCityWeather();
+  }
+  
+
+  function getCityWeather(response = ''){
+
+    setFirstApiCall(false);
+    let weatherApi='';
+    let key='6e4a909c74d0fa723ce663bd96696094';
+
+    if(typeof response == "object") {
+      // have not used set city here becuase city value does not reflect in this call and waits 
+      // until a new rerender to be applied, and this url is updated with with NULL instead of actual value,
+      weatherApi=`https://api.openweathermap.org/data/2.5/weather?q=${response.data.city}&appid=${key}&units=metric`;
+      console.log(city);
+    }
+    else
+      weatherApi=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    axios.get(weatherApi).then(function(response){
+      console.log(response);
+      setWeatherInfo(response.data);
+    }).catch(handleError);
+  }
+
+  function handleError(errorData){
+    setFirstApiCall(false);
+    console.log("Error: ");
+    console.log(errorData);
+    
+
+    if(city===''){
+      console.log("first if");
+      setError("Please do not leave field empty!");
+    }
+    else if(/[^A-Za-z_.'\- ]/.test(city)){
+      console.log("second if");
+      setError("Only letters and (- . ' _ ) symbols allowed!");
+    }
+    else if(Object.hasOwn(errorData, "response") && errorData.response.data.cod==="404"){
+        setError("City not found, please enter a valid city name!");
+    }
+    else{
+    setError('Sorry, something went wrong. Please try again later.');
+  }
+  }
+
+
+
+
   return (
   <div className="App">
-  <div className="main-container container ">
-  <div className="row">
-  <SearchForm />
-  </div>
-  <div className="row">
-  <div className="error"></div>
-  </div>
-  <span className="city-weather-details-container row">
-  <div className="city-details col">
-  <h1 id="city-name">Loading...</h1>
-  <p id="weather-condition">-</p>
-  <p id="current-time">00:00 AM GMT+00:00,</p>
-  <p id="current-date">-</p>
-  </div>
-  <div className="current-temp-container col">
-  <p id="current-temp"></p>
-  <img alt="current-weather-condition" id="current-weather-icon" src="assets/blank.png"  />
-  </div>
-  </span>
-  <div className="row">
-  <AddtionalDetialsContainer />
-  </div>
-  <div id="week-weather-forecast" className="row"></div>
-  </div>
-  <Credit />
-  <FooterLogo />
+    <div className="main-container container ">
+      <div className="row">
+        <form onSubmit={searchCity}>
+          <input
+            type="text"
+            id="search-field"
+            required
+            placeholder="Enter city name"
+            spellCheck="true"
+            pattern="^[a-zA-Z\s_\-]+$"
+            onInvalid = {handleError}
+            onChange = {updateCity}
+          />
+          <input type="submit" value="Search" id="submit-button" />
+        </form>
+      </div>
+      <div className="row">
+        {error===''?<div />:<div className="error-message">{error}</div>}
+      </div>
+      <span className="city-weather-details-container row">
+        <div className="city-details col">
+          {(weatherInfo!=null)?<span>
+            <h1 id="city-name">{weatherInfo.name}</h1> 
+            <p id="weather-condition">{weatherInfo.weather[0].description}</p> 
+            <FormatDate dateData = {weatherInfo.dt} />
+          </span>
+          : <span>
+          <h1 id="city-name">Loading...</h1> 
+          <p id="weather-condition">-</p> 
+          <p id="current-time">00:00 AM,</p>
+          <p id="current-date">-</p>
+        </span>}
+          
+        </div>
+        <div className="current-temp-container col">
+          <p id="current-temp">{weatherInfo!=null?Math.round(weatherInfo.main.temp): null}</p>
+          {weatherInfo==null? <WeatherIcon weatherConditionIcon = {null} alt = "Current Weather Condition" /> :<WeatherIcon weatherConditionIcon = {weatherInfo.weather[0].icon} alt = {weatherInfo.weather[0].description} /> }
+        </div>
+      </span>
+      <div className="row">
+        <AdditionalWeatherDetials weatherInfo = {weatherInfo!=null?weatherInfo:null} />
+      </div>
+      <div id="week-weather-forecast" className="row"></div>
+    </div>
+    <Credit />
+    <FooterLogo />
   </div>
   );
 }
